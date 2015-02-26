a6216784001bb86eaa95f8c90e71f141dc26e0ec
Conner Howell <28chowl28@gmail.com>
2015-02-26 14:19:31 -0500

    adding cors

--- public/javascripts/function.js
+++ public/javascripts/function.js
@@ -1,27 +1,27 @@
 $(document).ready(function(){
 	$("#search-form").submit(function(event) {
 		event.preventDefault();
-		search();
 	});
+	search();
 });
 
-function search(){
-	var listing = Handlebars.compile($('#listing').html());
-	var query = $("#searchbar").val();
-	var url = "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + query + "&format=json&srinfo&srlimit=50";
-	$.getJSON(url, function(data){
-		$('#content').html('<h1>Results</h1>');
-		//console.log(data);
-		for(i = 0; i < data.query.search.length; i++){
-			var html = listing({
-				title : data.query.search[i].title,
-				snippet : data.query.search[i].snippet
-			});
-			$('#content').append(data.query.search[i].snippet);
-			$('#content').append(html);
-		}
-	});
-}
+// function search(){
+// 	var listing = Handlebars.compile($('#listing').html());
+// 	var query = $("#searchbar").val();
+// 	var url = "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + query + "&format=json&srinfo&srlimit=50";
+// 	$.getJSON(url, function(data){
+// 		$('#content').html('<h1>Results</h1>');
+// 		//console.log(data);
+// 		for(i = 0; i < data.query.search.length; i++){
+// 			var html = listing({
+// 				title : data.query.search[i].title,
+// 				snippet : data.query.search[i].snippet
+// 			});
+// 			$('#content').append(data.query.search[i].snippet);
+// 			$('#content').append(html);
+// 		}
+// 	});
+// }
 function getWiki(query){
 	var url = "http://en.wikipedia.org/w/api.php?format=json&action=query&titles=" + query + "&prop=revisions&rvprop=content&rvparse";
 	$('#content').html('');
@@ -35,4 +35,33 @@ function getWiki(query){
 
 function first(obj){
 	for(var a in obj) return a;
+}
+
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+    // XHR for Chrome/Firefox/Opera/Safari.
+    xhr.open(method, url, true);
+  } else if (typeof XDomainRequest != "undefined") {
+    // XDomainRequest for IE.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+  } else {
+    // CORS not supported.
+    xhr = null;
+  }
+  return xhr;
+}
+
+function search(method, url){
+	var url = "http://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=wikipedia&srprop=timestamp&format=json";
+	var xhr = createCORSRequest('GET', url);
+	if (!xhr) {
+	  throw new Error('CORS not supported');
+	}
+	console.log(xhr);
+	xhr.onload = function(){
+		alert(xhr.responseText);
+	}
+	xhr.send();
 }
